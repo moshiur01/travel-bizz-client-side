d7ed215fe7d731be7b3fcb7b3c1d3bf1cdeb1b4e
moshiur01 <mr.moshiur314@gmail.com>
2021-11-01 18:59:44 +0600

    all data added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AuthProvider from "./Context/AuthProvider";
+import Header from "./Header/Header";
+import BlogDetails from "./Pages/Blogs/BlogDetails";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Footer from "./Pages/Footer/Footer";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login";
+import PrivateRoute from "./Pages/Login/PrivateRoute";
+import ManageOrders from "./Pages/ManageOrders/ManageOrders";
+import ManageServices from "./Pages/ManageServices/ManageServices";
+import MyOrders from "./Pages/MyOrders/MyOrders";
+
+import NotFound from "./Pages/NotFound/NotFound";
+
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+
+import ServiceDetails from "./Pages/Services/ServiceDetails";
+import WhyChooseUs from "./Pages/WhyChooseUs/WhyChooseUs";
 
 function App() {
+  const [handleConfirm, setHandleConfirm] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/login">
+              <Login></Login>
+            </Route>
+
+            <PrivateRoute exact path="/services/:id">
+              <ServiceDetails></ServiceDetails>
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/blogs/:id">
+              <BlogDetails></BlogDetails>
+            </PrivateRoute>
+
+            <Route exact path="/whyChooseUs">
+              <WhyChooseUs></WhyChooseUs>
+            </Route>
+            <Route exact path="/contactUs">
+              <ContactUs></ContactUs>
+            </Route>
+
+            <PrivateRoute exact path="/placeOrder/:id">
+              <PlaceOrder></PlaceOrder>
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/myOrders">
+              <MyOrders handleConfirm={handleConfirm}></MyOrders>
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/manageOrders">
+              <ManageOrders setHandleConfirm={setHandleConfirm}></ManageOrders>
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/manageServices">
+              <ManageServices></ManageServices>
+            </PrivateRoute>
+
+            <Route exact path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
 
---
+++ src/Context/Firebase/firebase.confiq.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcfLfPPyCyEBlrvEYTEK9uvJMbOKT0ZJU",
+  authDomain: "travel-bizz.firebaseapp.com",
+  projectId: "travel-bizz",
+  storageBucket: "travel-bizz.appspot.com",
+  messagingSenderId: "257316306175",
+  appId: "1:257316306175:web:7ef88ed1d15bbf15bd20ca",
+};
+
+export default firebaseConfig;
